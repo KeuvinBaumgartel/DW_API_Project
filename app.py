ca7956841f744b492e66f3d39e3aed964afb03f7
KeuvinBaumgartel <keuvin.baumgartel@grupokyly.com>
2025-05-16 14:32:37 -0300

    Preparando para deploy no Vercel

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 from fastapi import FastAPI
-from sqlalchemy.orm import Session
-from db_connection import get_db
+from db_connection import get_db_session
 from models import Pedido
-from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
 app = FastAPI()
 
-# Habilitando CORS para permitir conexões externas
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 @app.get("/pedidos", response_model=List[Pedido])
-def get_pedidos(db: Session = get_db()):
-    try:
-        pedidos = db.query(Pedido).limit(50).all()
-        return pedidos
-    except Exception as e:
-        return {"error": f"Erro ao buscar pedidos: {str(e)}"}
+async def get_pedidos():
+    db = get_db_session()
+    pedidos = db.query(Pedido).limit(50).all()
+    return pedidos
