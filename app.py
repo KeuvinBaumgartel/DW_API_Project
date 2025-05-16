80f2042852f8d0d7edd9876538cedad205cb9b4c
KeuvinBaumgartel <keuvin.baumgartel@grupokyly.com>
2025-05-16 14:39:09 -0300

    Preparando para deploy no Vercel

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from sqlalchemy.orm import Session
 from db_connection import get_db_session
 from models import Pedido
 from typing import List
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/pedidos", response_model=List[Pedido])
+# Definindo o esquema Pydantic para a resposta
+class PedidoResponse(BaseModel):
+    pedido: str
+    data_pedido: str
+    pedido_externo: str
+    pedido_cliente: str
+    tipo_pedido: str
+    filial: str
+    canal: str
+    cod_cliente: str
+    transportadora: str
+    moeda: str
+    tot_valor_original: float
+    tot_valor_entregue: float
+    tot_valor_cancelado: float
+    desconto: float
+
+    class Config:
+        orm_mode = True
+
+@app.get("/pedidos", response_model=List[PedidoResponse])
 async def get_pedidos():
-    db = get_db_session()
-    pedidos = db.query(Pedido).limit(50).all()
-    return pedidos
+    try:
+        db: Session = get_db_session()
+        pedidos = db.query(Pedido).limit(50).all()
+        db.close()  # Fechando a sessão para evitar problemas
+        return pedidos
+    except Exception as e:
+        db.close()  # Garante que a sessão seja fechada mesmo com erro
+        raise HTTPException(status_code=500, detail=str(e))
